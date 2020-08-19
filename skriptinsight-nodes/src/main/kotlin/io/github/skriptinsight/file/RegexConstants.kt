342369dfb5a40c1d4d00d7eb22a6da04f33cc3be
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-08-19 20:48:26 +0100

    Include more patterns from the Skript codebase

--- skriptinsight-nodes/src/main/kotlin/io/github/skriptinsight/file/RegexConstants.kt
+++ skriptinsight-nodes/src/main/kotlin/io/github/skriptinsight/file/RegexConstants.kt
@@ -1,5 +1,23 @@
 package io.github.skriptinsight.file
 
+import org.intellij.lang.annotations.Language
 import java.util.regex.Pattern
 
 val linePattern: Pattern = Pattern.compile("^((?:[^#]|##)*)(\\s*#(?!#).*)$")
+
+const val functionNamePattern = "[\\p{IsAlphabetic}][\\p{IsAlphabetic}\\p{IsDigit}_]*";
+val functionNamePatternRegex = Pattern.compile(functionNamePattern)
+
+val functionPattern = Pattern.compile("function ($functionNamePattern)\\((.*)\\)(?: :: (.+))?", Pattern.CASE_INSENSITIVE)
+
+val paramPattern = Pattern.compile("\\s*(.+?)\\s*:(?=[^:]*$)\\s*(.+?)(?:\\s*=\\s*(.+))?\\s*")
+
+val commandPattern = Pattern.compile("(?i)^command /?(\\S+)\\s*(\\s+(.+))?$")
+
+@Language("regexp")
+const val wildcard = "[^\"]*?(?:\"[^\"]*?\"[^\"]*?)*?"
+
+@Language("regexp")
+const val stringMatcher = "\"[^\"]*?(?:\"\"[^\"]*)*?\""
+
+val argumentPattern = Pattern.compile("<\\s*(?:(.+?)\\s*:\\s*)?(.+?)\\s*(?:=\\s*($wildcard))?\\s*>")
