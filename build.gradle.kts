57d312cb220a988b6053bb762f718ec7a4a819df
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-08-20 23:04:08 +0100

    Actually fix build not having artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    kotlin("jvm") version "1.3.72" apply false
+    kotlin("jvm") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "6.0.0"
     `maven-publish`
 }
 
+allprojects {
+    group = "io.github.skriptinsight"
+    version = "0.1-SNAPSHOT"
+}
 
 subprojects {
     apply {
@@ -12,12 +16,10 @@ subprojects {
         plugin("org.gradle.maven-publish")
     }
 
-    group = "io.github.skriptinsight"
-    version = "0.1-SNAPSHOT"
-
     repositories {
         mavenCentral()
     }
+
     val shadow by configurations
     val testImplementation by configurations
 
@@ -34,14 +36,6 @@ subprojects {
         }
     }
 
-    publishing {
-        publications {
-            create("shadow", MavenPublication::class.java) {
-                project.shadow.component(this)
-            }
-        }
-    }
-
     // config JVM target to 11 for kotlin compilation tasks
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions.jvmTarget = "11"
@@ -51,5 +45,34 @@ subprojects {
         classifier = null
     }
 
+    publishing {
+        publications {
+            create("shadow", MavenPublication::class.java) {
+                project.shadow.component(this)
+            }
+        }
+    }
 }
 
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test> {
+    outputs.upToDateWhen { false }
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    classifier = null
+
+    relocate("kotlin", "${project.group}.depends.kotlin")
+    relocate("org.intellij", "${project.group}.depends.intellij")
+    relocate("org.jetbrains", "${project.group}.depends.jetbrains")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    subprojects.forEach {
+        implementation(it)
+    }
+}
